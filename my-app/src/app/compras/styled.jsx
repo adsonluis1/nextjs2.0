f08b74af36dad5cee45e384b6a7c5a900e1b636d
adsonluis1 <dev.adsonluis@gmail.com>
2024-05-20 23:31:08 -0300

    styled de compras

---
+++ my-app/src/app/compras/styled.jsx
@@ -0,0 +1,15 @@
+'use client'
+import styled from 'styled-components'
+
+export const Section = styled.section`
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    align-items: center;
+    flex-wrap: wrap;
+    gap: 20px;
+    background-color: black;
+    padding: 8px;
+    margin-top: 10px;
+    width: 100%;
+`
