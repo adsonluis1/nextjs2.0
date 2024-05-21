eecc0f9f21f5ca6e6bc0209bda3b2f201b6ad364
adsonluis1 <dev.adsonluis@gmail.com>
2024-05-21 10:47:38 -0300

    useGetItens

---
+++ my-app/src/hooks/useGetItens.jsx
@@ -0,0 +1,12 @@
+export async function useGetItens(){
+    try {
+        const get = await fetch('http://localhost:3000/itens',
+            {method:'GET'}
+        )
+        const data =  await get.json()        
+        return await data
+    } catch (error) {
+        console.log(error)
+    }
+    
+} 
