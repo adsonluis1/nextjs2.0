64a5873f18fc4a1e85e41f67959fc966e931ff51
adsonluis1 <dev.adsonluis@gmail.com>
2024-05-20 23:37:11 -0300

    20/05

---
+++ my-app/src/hooks/Context.jsx
@@ -0,0 +1,15 @@
+'use client'
+
+import { createContext, useContext, useState } from "react"
+
+export const Context = createContext()
+
+export const ContextProviber = ({children})=>{
+    const [carrinho, setCarrinho] = useState([])
+    return <Context.Provider value={{carrinho, setCarrinho}}>{children}</Context.Provider>
+}
+
+export const useAppContext = ()=>{
+    const data = useContext(Context)
+    return data
+}
