be5a3d10ffe0340b4ceaefdda2bbc912451e33a2
adsonluis1 <dev.adsonluis@gmail.com>
2024-05-22 10:50:49 -0300

    carrinho

--- my-app/src/app/carrinho/page.jsx
+++ my-app/src/app/carrinho/page.jsx
@@ -1,19 +1,23 @@
+'use client'
 import { useGetItensCar } from '@/hooks/useGetItensCar'
 import * as S from './styled'
 import CarrinhoItens from '@/components/carrinhoItens/page'
+import { useAppContext } from '@/hooks/Context'
 
 
-const page =  async () => {  
-  const itens = await useGetItensCar()
-  console.log(itens)
+const Page = () => {  
+  // const itens = await useGetItensCar()
+  // console.log(itens)
+  const {carrinho, setCarrinho} = useAppContext()
+  console.log(carrinho)
   return (
     <S.Section>
-      <S.H1>Carrinho</S.H1>
-      {itens.map((item)=>[
-        <CarrinhoItens item={item}/>
+      {/* <S.H1>Carrinho</S.H1> */}
+      {carrinho.length >= 1 && carrinho.map((item)=>[
+        <CarrinhoItens key={item.id} item={item}/>
       ])}
     </S.Section>
   )
 }
 
-export default page
+export default Page
