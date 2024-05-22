df0b301a91d3e5c494500e97aa8b1bb52632300f
adsonluis1 <dev.adsonluis@gmail.com>
2024-05-22 10:53:08 -0300

    cardItens

--- my-app/src/components/cardItens/page.jsx
+++ my-app/src/components/cardItens/page.jsx
@@ -2,21 +2,31 @@
 import { useState } from 'react'
 import * as S from './styled'
 import { useAddCar } from '@/hooks/useAddCar'
+import { useAppContext } from '@/hooks/Context'
+
 
 const CardItens= ({produc}) => {
   const [quantidade, setQuatidade] = useState(1)
+  const {carrinho, setCarrinho} = useAppContext()
   let newCompra = {...produc, quantidade}
   
+
   return (
-    <S.divConteiner>
-      <S.img src={produc.img} alt={produc.nome}/>
-      <S.divChild>
-        <S.heading>{produc.nome}</S.heading>
-        <S.price>R$:{produc.price}</S.price>
-        <S.radioQuant type='number' min={1} max={100} value={quantidade} onChange={((e)=>{setQuatidade(e.target.value)})}  placeholder='Quantidade'/>
-      </S.divChild>
-      <S.input defaultValue='Comprar' onClick={()=> useAddCar({newCompra})}type='button'/>
-    </S.divConteiner>
+    <>
+      <S.divConteiner>
+        <S.img src={produc.img} alt={produc.nome}/>
+        <S.divChild>
+          <S.heading>{produc.nome}</S.heading>
+          <S.price>R$:{produc.price}</S.price>
+          <S.radioQuant type='number' min={1} max={100} value={quantidade} onChange={((e)=>{setQuatidade(e.target.value)})}  placeholder='Quantidade'/>
+        </S.divChild>
+        <S.input value='Comprar' onClick={()=> {
+          setCarrinho((prev)=> [...prev,newCompra])
+          console.log(carrinho)
+          console.log('comprado')
+        }}type='button'/>
+      </S.divConteiner>
+    </>
   )
 }
 
